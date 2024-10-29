f8f92513d746525801af093366f7d7dc8f87574f
@Mikist <mikeochango3@gmail.com>
2024-10-29 11:28:20 +0300

    working with templates

--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("Hello Mikes")
+    # return HttpResponse("Hello Mikes")
+    # return render(request, 'hello.html')
+     return render(request, 'hello.html', {'name':"Absai"})
