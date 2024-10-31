e3161108af327ed0b90075d628e69f0402415907
@Mikist <mikeochango3@gmail.com>
2024-10-31 12:08:13 +0300

    configured the database

--- alx_practice/alx_practice/settings.py
+++ alx_practice/alx_practice/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'alx_practice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'dbalxpractice',
+        'USER': 'root',
+        'PASSWORD': '@Mikist@23.@Mikist@23.',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
