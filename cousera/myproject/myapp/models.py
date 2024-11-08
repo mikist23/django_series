941bfbbb50ace04b6f0d343bb24b75dfb1ae1f43
@Mikist <mikeochango3@gmail.com>
2024-11-08 20:07:29 +0300

    working with python shell

--- cousera/myproject/myapp/models.py
+++ cousera/myproject/myapp/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class DrinksCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+
 class Drinks(models.Model):
     drink = models.CharField(max_length=200)
     prices = models.IntegerField()
+    category_id = models.ForeignKey(DrinksCategory, on_delete=models.PROTECT, default=None)
+    
     
 
     def __str__(self):
