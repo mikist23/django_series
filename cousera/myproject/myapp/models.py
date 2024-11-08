2b86ee82564159fba9d39bbeb1827c398ac589d7
@Mikist <mikeochango3@gmail.com>
2024-11-08 15:18:32 +0300

    learn about models  and migrationns

--- cousera/myproject/myapp/models.py
+++ cousera/myproject/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Drinks(models.Model):
+    drink = models.CharField(max_length=200)
+    prices = models.IntegerField()
+    
+
+    def __str__(self):
+        return super().__str__()
