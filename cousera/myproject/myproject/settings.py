fc00474436bdaa26a052722173b9675654a44074
@Mikist <mikeochango3@gmail.com>
2024-11-09 13:22:47 +0300

    Databse connection

--- cousera/myproject/myproject/settings.py
+++ cousera/myproject/myproject/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': '@Mikist@23.@Mikist@23.',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
