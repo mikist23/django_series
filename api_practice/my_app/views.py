441cba43f244d27872c11f80940a4ac8b05d424c
@Mikist <mikeochango3@gmail.com>
2024-11-26 17:28:53 +0300

    configure the dtabase structure

--- api_practice/my_app/views.py
+++ api_practice/my_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializers
+
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializers
