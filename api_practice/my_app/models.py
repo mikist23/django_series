cfd5d6807b76f763971ef84390425d3c61eadb26
@Mikist <mikeochango3@gmail.com>
2024-11-27 12:54:35 +0300

    implement serializaion for the post model

--- api_practice/my_app/models.py
+++ api_practice/my_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- api_practice/my_app/serializers.py
+++ api_practice/my_app/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Post
 
 class BookSerializers(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ['id','title','content','author','created_at']
